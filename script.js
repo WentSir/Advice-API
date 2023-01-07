03a0a177e4e58eeed165a5a23c60fdffef849837
Ismail Ibrahim <opeyemi942@gmail.com>
2023-01-07 17:30:16 +0100

    advice api

---
+++ script.js
@@ -0,0 +1,32 @@
+var search = document.getElementById("search");
+var btn = document.getElementById("click")
+var output = document.getElementById("output");
+var body = document.getElementById("body");
+
+
+body.onload = function(){
+   
+        fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((response) => output.innerText = (response.slip.advice))
+}
+
+
+btn.onclick = function(){
+    if(search.value === ""){
+    alert("Search field is empty")
+
+    }else{
+        fetch(`https://api.adviceslip.com/advice/search/${search.value}`)
+        .then((response) => response.json())
+        .then((response) => {
+            if(response.message){
+                alert(response.message.text)
+            }else{
+                var res = response.slips[Math.floor(Math.random() * response.slips.lenght - 1) ]
+                output.innerText = (res.advice)
+            }
+        })
+    }
+
+}
